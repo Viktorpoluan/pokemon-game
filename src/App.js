e81739da6dd430b70d322c774962752f3d0cd4ef
Viktorpolan <viktorpolan987654321@gmail.com>
2021-01-28 21:22:31 +0300

    homework2_2
    really stylish man...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
-import bg1 from './components/images/bg1.jpg'
+import pokemon1 from './components/images/pokemon1.jpg'
 
 
 const App = () => {
     return (
         <div>
-            <Header title={'This is title'} desc={'This is Description!'}/>
-            <Layout id={1} title={'title 1'} desc={'description 1'} urlBg={bg1}/>
+            <Header title={'Pokemon amazing!'} desc={'This is card game'}/>
+            <Layout id={1} title={'title 1'} desc={'description 1'} urlBg={pokemon1}/>
             <Layout id={2} title={'title 2'} desc={'description 2'} colorBg={'skyblue'}/>
-            <Layout id={3} title={'title 3'} desc={'description 3'} urlBg={bg1}/>
+            <Layout id={3} title={'title 3'} desc={'description 3'} urlBg={pokemon1}/>
             <Footer/>
         </div>
     )
